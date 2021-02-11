211fb61283d7e7abb079bc0b24811fbd3a6ba8e4
João Gabriel Dourado Cervo <joaogabrieldouradocervo@gmail.com>
2021-02-11 08:20:34 -0300

    ADD labels para colocar o conteudo

--- hangman/ViewController.swift
+++ hangman/ViewController.swift
@@ -8,10 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var wordLabel: UILabel!
+    var numberOfTriesLabel: UILabel!
+    
+    let correctAnswer: String = "?"
+    var numberOfTriesRemaining = 5 {
+        didSet {
+            numberOfTriesLabel.text = "\(numberOfTriesRemaining) tries remaining"
+        }
+    }
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+        
+        wordLabel = UILabel()
+        wordLabel.translatesAutoresizingMaskIntoConstraints = false
+        wordLabel.textAlignment = .center
+        wordLabel.font = UIFont.systemFont(ofSize: 24)
+        wordLabel.text = "?"
+        view.addSubview(wordLabel)
+        
+        numberOfTriesLabel = UILabel()
+        numberOfTriesLabel.translatesAutoresizingMaskIntoConstraints = false
+        numberOfTriesLabel.textAlignment = .center
+        numberOfTriesLabel.font = UIFont.systemFont(ofSize: 20)
+        numberOfTriesLabel.text = "\(numberOfTriesRemaining) tries remaining"
+        view.addSubview(numberOfTriesLabel)
+        
+        let submitButton = UIButton(type: .system)
+        submitButton.translatesAutoresizingMaskIntoConstraints = false
+        submitButton.setTitle("New letter", for: .normal)
+        view.addSubview(submitButton)
+            
+        // testing
+        numberOfTriesLabel.backgroundColor = .red
+        wordLabel.backgroundColor = .blue
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
     }
 
 
